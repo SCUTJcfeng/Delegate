27bfcb2fc92c1882246f36c3d813d8d506ec6965
SCUTJcfeng <jcfeng2013@gmail.com>
2017-08-23 13:36:26 +0800

    Button02
    Use Delegate and Event, in downloading--Show Message.
    Use Timer.
    Use ProgressBar.

---
+++ Button02/Button02/Form1.Designer.cs
@@ -0,0 +1,130 @@
+﻿namespace Button02
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.btnStart = new System.Windows.Forms.Button();
+            this.btnPause = new System.Windows.Forms.Button();
+            this.btnStop = new System.Windows.Forms.Button();
+            this.progressBar = new System.Windows.Forms.ProgressBar();
+            this.tmr = new System.Windows.Forms.Timer(this.components);
+            this.label1 = new System.Windows.Forms.Label();
+            this.lbPercentage = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // btnStart
+            // 
+            this.btnStart.Location = new System.Drawing.Point(57, 238);
+            this.btnStart.Name = "btnStart";
+            this.btnStart.Size = new System.Drawing.Size(75, 23);
+            this.btnStart.TabIndex = 0;
+            this.btnStart.Text = "Start";
+            this.btnStart.UseVisualStyleBackColor = true;
+            this.btnStart.Click += new System.EventHandler(this.btnStart_Click);
+            // 
+            // btnPause
+            // 
+            this.btnPause.Location = new System.Drawing.Point(226, 238);
+            this.btnPause.Name = "btnPause";
+            this.btnPause.Size = new System.Drawing.Size(75, 23);
+            this.btnPause.TabIndex = 1;
+            this.btnPause.Text = "Pause";
+            this.btnPause.UseVisualStyleBackColor = true;
+            this.btnPause.Click += new System.EventHandler(this.btnPause_Click);
+            // 
+            // btnStop
+            // 
+            this.btnStop.Location = new System.Drawing.Point(395, 238);
+            this.btnStop.Name = "btnStop";
+            this.btnStop.Size = new System.Drawing.Size(75, 23);
+            this.btnStop.TabIndex = 2;
+            this.btnStop.Text = "Stop";
+            this.btnStop.UseVisualStyleBackColor = true;
+            this.btnStop.Click += new System.EventHandler(this.btnStop_Click);
+            // 
+            // progressBar
+            // 
+            this.progressBar.Location = new System.Drawing.Point(57, 114);
+            this.progressBar.Name = "progressBar";
+            this.progressBar.Size = new System.Drawing.Size(413, 70);
+            this.progressBar.TabIndex = 3;
+            // 
+            // tmr
+            // 
+            this.tmr.Tick += new System.EventHandler(this.tmr_Tick);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(55, 200);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(119, 12);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "Current Percentage:";
+            // 
+            // lbPercentage
+            // 
+            this.lbPercentage.AutoSize = true;
+            this.lbPercentage.Location = new System.Drawing.Point(180, 200);
+            this.lbPercentage.Name = "lbPercentage";
+            this.lbPercentage.Size = new System.Drawing.Size(11, 12);
+            this.lbPercentage.TabIndex = 5;
+            this.lbPercentage.Text = "0";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(617, 377);
+            this.Controls.Add(this.lbPercentage);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.progressBar);
+            this.Controls.Add(this.btnStop);
+            this.Controls.Add(this.btnPause);
+            this.Controls.Add(this.btnStart);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnStart;
+        private System.Windows.Forms.Button btnPause;
+        private System.Windows.Forms.Button btnStop;
+        private System.Windows.Forms.ProgressBar progressBar;
+        private System.Windows.Forms.Timer tmr;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label lbPercentage;
+    }
+}
+
---
+++ Button02/Button02/Form1.cs
@@ -0,0 +1,136 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Button02
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            progressBar.Minimum = 0;
+            progressBar.Maximum = 100;
+            progressBar.Value = 0;
+            Start += StartShowMessage;
+            Pause += PauseShowMessage;
+            Stop += StopShowMessage;
+
+            btnStart.Enabled = true;
+            btnPause.Enabled = false;
+            btnStop.Enabled = false;
+
+            lbPercentage.Text = progressBar.Value.ToString();
+        }
+
+        #region delegate
+        public delegate void StartEventHandler(object sender, StartEventArgs e);
+
+        public event StartEventHandler Start;
+
+        public class StartEventArgs : EventArgs
+        {
+            public int percent;
+            public StartEventArgs(int percent)
+            {
+                this.percent = percent;
+            }
+        }
+
+        public delegate void PauseEventHandler(object sender, PauseEventArgs e);
+
+        public event PauseEventHandler Pause;
+
+        public class PauseEventArgs : EventArgs
+        {
+            public int percent;
+            public PauseEventArgs(int percent)
+            {
+                this.percent = percent;
+            }
+        }
+
+        public delegate void StopEventHandler(object sender, StopEventArgs e);
+
+        public event StopEventHandler Stop;
+
+        public class StopEventArgs : EventArgs
+        {
+            public int percent;
+            public StopEventArgs(int percent)
+            {
+                this.percent = percent;
+            }
+        }
+        #endregion
+
+        private void btnStart_Click(object sender, EventArgs e)
+        {
+            Start(this, new StartEventArgs(progressBar.Value));
+            tmr.Enabled = true;
+            btnStart.Enabled = false;
+            btnPause.Enabled = true;
+            btnStop.Enabled = true;
+        }
+
+        private void btnPause_Click(object sender, EventArgs e)
+        {
+            tmr.Enabled = false;
+            btnStart.Enabled = true;
+            btnPause.Enabled = false;
+            Pause(this, new PauseEventArgs(progressBar.Value));
+        }
+
+        private void btnStop_Click(object sender, EventArgs e)
+        {
+            tmr.Enabled = false;
+            btnStop.Enabled = false;
+            btnStart.Enabled = true;
+            btnPause.Enabled = true;
+            Stop(this, new StopEventArgs(progressBar.Value));
+            progressBar.Value = 0;
+            lbPercentage.Text = progressBar.Value.ToString();
+        }
+
+        public void StartShowMessage(object sender, StartEventArgs e)
+        {
+            MessageBox.Show("Download Starts, Initial Percentage is " + e.percent);
+        }
+
+        public void PauseShowMessage(object sender, PauseEventArgs e)
+        {
+            MessageBox.Show("Download Pauses, Current Percentage is " + e.percent);
+        }
+
+        public void StopShowMessage(object sender, StopEventArgs e)
+        {
+            MessageBox.Show("Download Stops, Final Percentage is " + e.percent);
+        }
+
+        private void tmr_Tick(object sender, EventArgs e)
+        {
+            if (progressBar.Value < progressBar.Maximum)
+                progressBar.Value++;
+            lbPercentage.Text = progressBar.Value.ToString();
+            if (progressBar.Value == 100)
+            {
+                btnStart.Enabled = true;
+                btnPause.Enabled = false;
+                btnStop.Enabled = false;
+                tmr.Enabled = false;
+                Stop(this, new StopEventArgs(progressBar.Value));
+                progressBar.Value = 0;
+                lbPercentage.Text = progressBar.Value.ToString();
+
+            }
+        }
+
+       
+    }
+}
